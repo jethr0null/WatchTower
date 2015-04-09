3812930792de6d70fddf61535e62671c0b8ae064
Michelle Sausa <mnikki137@gmail.com>
2015-04-09 15:49:05 -0600

    Added customer model/controller.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409193904) do
+ActiveRecord::Schema.define(version: 20150409212532) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
     t.integer  "account_number"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
