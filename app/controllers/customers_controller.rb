297693e2fd1c1ff038769372e0ebca85fb31dd0a
Michelle Sausa <mnikki137@gmail.com>
2015-04-14 15:31:33 -0600

    Add dashboard view to customers and delete static page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -67,6 +67,10 @@ class CustomersController < ApplicationController
     end
   end
 
+  def dashboard
+    @customer = Customer.find(params[:format])
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_customer
