04dce775dabbf814279f00027de74b2c89d78880
Michelle Sausa <mnikki137@gmail.com>
2015-04-17 10:39:37 -0600

    Change formatting of login error notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def confirm_logged_in
      unless session[:user_id]
-         flash[:notice] = "Please log in"
+         flash[:danger] = "Please log in"
          redirect_to :root
          return false
      else
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,5 @@
 class StaticPagesController < ApplicationController
   before_filter :confirm_logged_in
-  def home
-  end
 
   def dashboard
   end
